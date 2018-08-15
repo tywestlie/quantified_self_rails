a5648279e9a142417b387a85dfcd7ead3b79a58c
Tyler Westlie <tyler.westlie@gmail.com>
2018-08-14 21:57:57 -0600

    added seed data to the deployed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 meal1 = Meal.create(name: 'Breakfast')
 meal1.foods.create(name: 'Bacon', calories: 500)
 meal1.foods.create(name: 'More Bacon', calories: 700)
